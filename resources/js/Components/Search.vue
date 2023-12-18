edfdc3619fd370484ad20d6e55948bd058478a0e
curder <q.curder@gmail.com>
2023-12-18 14:20:15 +0800

    Setting up autocomplete

---
+++ resources/js/Components/Search.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {autocomplete} from "@algolia/autocomplete-js";
+import {onMounted} from "vue";
+
+onMounted(() => {
+
+    autocomplete({
+        container: "#search",
+        placeholder: "您向要搜索什么?",
+        autoFocus: true,
+        getSources() {
+            return [
+                {
+                    sourceId: 'users',
+                    getItems({query}) {
+                        let items = [
+                            {id: 1, name: 'Curder'},
+                            {id: 2, name: 'Li Lei'},
+                        ];
+                        return items.filter(({name}) => name.toUpperCase().includes(query.trim().toUpperCase()));
+                    },
+                    templates: {
+                        item({item}) {
+                            return item.name;
+                        }
+                    }
+                }
+            ];
+        },
+    })
+})
+</script>
+
+<template>
+    <div id="search"></div>
+</template>
